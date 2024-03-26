3c5276116a5e61c7d304549b9bdb4e6a6f19b558
Seif Elredeini <ojack7666@gmail.com>
2024-03-26 15:33:29 +0300

    Updated comments.php & content-article.php

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,5 +1,19 @@
-<?php
+<div class="container">
+    <header class="content-header">
+        <div class="meta mb-3">
+            <span class="date"><?php the_date(); ?></span>
+            <?php
+               the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>');
+            ?>
+            <span class="comment"><a href="#comments"><i class='fa fa-comment'></i><? comments_number(); ?></a> </span>
+        </div>
+    </header>
 
+    <?php
 the_content();
-
 ?>
+
+<?php   
+comments_template();
+?>
+</div>
