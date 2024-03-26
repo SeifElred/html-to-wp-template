01f2c15693a9add3451f9ce31e9e5759c0c86066
Seif Elredeini <ojack7666@gmail.com>
2024-03-26 13:55:47 +0300

    Changed Name Tags

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer text-center py-2 theme-bg-dark">
 		   
-           <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+           <p class="copyright"><a href="https://seifelredeini.com/">Follow Seif Elredeini</a></p>
           
        </footer>
    
--- header.php
+++ header.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
-    <meta name="author" content="https://youtube.com/FollowAndrew">
-    <link rel="shortcut icon" href="/wp-content/themes/seifelred/assets/images/logo.png">
+    <meta name="author" content="https://seifelredeini.com/">
+    <link rel="shortcut icon" href="/wp-content/themes/html-to-wp-template/assets/images/logo.png">
     
 	<?php
     	wp_head();
